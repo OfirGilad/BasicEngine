c028e1342dbacdb6e6a022297260e54d8bb6dd65
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2022-12-05 14:30:13 +0200

    fixed checkerboard

--- Engine3D/utilities.cpp
+++ Engine3D/utilities.cpp
@@ -94,23 +94,34 @@ float Plane::FindIntersection(vec3 ray, vec3 somePointOnRay) {
 	float vecy = ray.y;
 	float vecz = ray.z;
 
-	float ans = -(a * x0 + b * y0 + c * z0 + d) / (a * vecx + b * vecy + c * vecz); // = -(dot(planeNormal, somePointOnRay) + d) / dot(planeNormal, ray);
-
+	//float ans = -(a * x0 + b * y0 + c * z0 + d) / (a * vecx + b * vecy + c * vecz); // = -(dot(planeNormal, somePointOnRay) + d) / dot(planeNormal, ray);
+	float ans = INFINITY;
+	if (abs(dot(ray, planeNormal)) > 0.0) {
+		ans = -(dot(somePointOnRay, planeNormal) + d) / dot(ray, planeNormal);
+	}
 	return ans;
 }
 
 vec3 Plane::getColor(vec3 hitPoint) {
 	//float angle = this->getAngle(ray, hitPoint);
 
-	// checkers board pattern
 	//if ((int(1.5 * hitPoint.x) % 2) == (int(1.5 * hitPoint.y) % 2)) {
 	//	return 0.5f * this->rgb_color;
 	//}
-	if (((((int(1.5 * hitPoint.x) % 2) == (int(1.5 * hitPoint.y) % 2)) && ((hitPoint.x > 0 && hitPoint.y > 0) || (hitPoint.x < 0 && hitPoint.y < 0)))) ||
-		((((int(1.5 * hitPoint.x) % 2) != (int(1.5 * hitPoint.y) % 2)) && ((hitPoint.x < 0 && hitPoint.y > 0) || (hitPoint.x > 0 && hitPoint.y < 0))))) {
+
+	//if (((((int(1.5 * hitPoint.x) % 2) == (int(1.5 * hitPoint.y) % 2)) && ((hitPoint.x > 0 && hitPoint.y > 0) || (hitPoint.x < 0 && hitPoint.y < 0)))) ||
+	//	((((int(1.5 * hitPoint.x) % 2) != (int(1.5 * hitPoint.y) % 2)) && ((hitPoint.x < 0 && hitPoint.y > 0) || (hitPoint.x > 0 && hitPoint.y < 0))))) {
+	//	return 0.5f * this->rgb_color;
+	//}
+	
+	// Checkerboard pattern
+	float scaler_parameter = 0.6f;
+	float chessboard = floor((hitPoint.x + 2.0f) / scaler_parameter) + floor((hitPoint.y + 2.0f) / scaler_parameter);
+	chessboard = (chessboard * 0.5) - int(chessboard * 0.5);
+	chessboard *= 2;
+	if (chessboard > 0.5) {
 		return 0.5f * this->rgb_color;
 	}
-
 	return this->rgb_color; // I = I(emission) + K(ambient) * I(ambient) + K(diffuse) * (N dot L) * I(light intensity) + K(specular) * (V dot R)^n * I(light intensity)
 }
 
