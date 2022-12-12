93354f8a2f4996d15b5b377e5c0e50c1406d6fbc
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2022-12-12 22:46:41 +0200

    added assignment files

---
+++ Engine3D/assignment.cpp
@@ -0,0 +1,10 @@
+#include "assignment.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "../Game/game.h"
+
+using namespace std;
+using namespace glm;
---
+++ Engine3D/assignment.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../res/includes/glm/glm.hpp"
+
+using namespace std;
+using namespace glm;
+
