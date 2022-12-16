8e226976f2b99c69a07ee2a642fdc0ed639d3fab
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2022-12-16 16:43:42 +0200

    create cubes of all sizes works

--- Engine3D/assignment.cpp
+++ Engine3D/assignment.cpp
@@ -14,10 +14,26 @@ RubiksCube::RubiksCube(Game* scn, int size) {
     this->size = size;
 
     scn->AddTexture("../res/textures/plane.png", false);
-    scn->AddShape(Scene::Cube, -1, Scene::TRIANGLES);
-    scn->SetShapeTex(0, 0); 
 
     scn_shapes = scn->getShapes();
     //(*scn_shapes)[0]->MyScale(glm::vec3(0.5, 0.5, 0.5));
-    //(*scn_shapes)[0]->MyTranslate(glm::vec3(0, 0, 1), 1);
+    //(*scn_shapes)[0]->MyTranslate(glm::vec3(1, 0, 0), 0);
+    //(*scn_shapes)[0]->MyRotate(60, glm::vec3(0, 1, 0), 0);
+
+    float distance = float(size) - 0.5f;
+    int index = 0;
+
+    for (float i = -distance; i <= distance; i+=2)
+    {
+        for (float j = -distance; j <= distance; j+=2)
+        {
+            for (float k = -distance; k <= distance; k+=2)
+            {
+                scn->AddShape(Scene::Cube, -1, Scene::TRIANGLES);
+                scn->SetShapeTex(index, 0);
+                (*scn_shapes)[index]->MyTranslate(glm::vec3(i, j, k), 0);
+                index++;
+            }
+        }
+    }
 }
